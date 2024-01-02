f3405c7013d1184b64446a3252571c76d5e4298e
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-02 16:41:56 +0300

    Add LIFOCache class that implements a caching system with a Last-In, First-Out algorithm

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+This module contains a class LIFOCache that inherits from BaseCaching.
+This caching system uses a Last-In, First-Out algorithm.
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache class. It is a caching system with a Last-In, First-Out
+    algorithm.
+    It inherits from BaseCaching and overrides put and get methods.
+    """
+    def __init__(self):
+        """
+        Initialize the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Assign the item value for the key in self.cache_data dictionary.
+        If the key or item is None or an empty string, this method should
+        not do anything.
+        If the number of items in self.cache_data is higher than
+        BaseCaching.MAX_ITEMS,
+        it must discard the last item put in cache (LIFO algorithm).
+        """
+        if not key or not item:
+            return
+        if (
+            key not in self.cache_data and len(
+                self.cache_data
+            ) >= BaseCaching.MAX_ITEMS
+        ):
+            last_key, _ = self.cache_data.popitem(last=True)
+            print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key.
+        If key is None or an empty string, or if the key doesn't exist
+        in self.cache_data, return None.
+        """
+        return self.cache_data.get(key, None)
