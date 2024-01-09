f01aa28cb5b863528aa61084463a5c3403c23a49
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-09 13:34:12 +0300

    Add Flask app with user login system and corresponding template

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+A flask app with Babel config
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user():
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+@app.route('/')
+def index():
+    """
+    Home Page
+    """
+    return render_template('5-index.html')
+
+@babel.localeselector
+def get_locale():
+    if g.user:
+        return g.user['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
