8cb22f14c396696cb733e6c223a9a6a57fa18633
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-12 15:49:48 +0300

    fixed: default timezone - alx checker tool qa review

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,7 +12,7 @@ class Config(object):
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEONE = 'UTC'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
