a0eca74fa556afe36cf76cb1df88d3c8dd003716
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-12 15:54:19 +0300

    fixed: Parameterized template task

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,6 +7,9 @@ from flask_babel import Babel
 
 
 class Config(object):
+    """
+    Flask Babel configuration representation.
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
